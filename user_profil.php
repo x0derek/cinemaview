02a3efbc45ad208e88873351feacf2212ef75e2b
Igor Karpowicz <i.karpowicz09@gmail.com>
2026-02-06 12:46:02 +0100

    zaktualizowano pliki

---
+++ user_profil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel użytkownika</title>
+</head>
+<body>
+    <header>
+        <p>Jesteś zalogowany na swoim panelu</p>
+    </header>
+    <p>tutaj nawigacja</p>
+    <main>
+        <div>
+            tutaj imie nazwisko
+        </div>
+        <div>
+            zamowione bilety
+        </div>
+        <div>
+            liczba kupionych biletów
+        </div>
+    </main>
+</body>
+</html>
