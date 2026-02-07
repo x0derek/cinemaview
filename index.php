a594a0cb9ff7ce44ecddad89c69d2f69739489e4
x0derus <i.karpowicz09@gmail.com>
2026-02-07 21:11:06 +0100

    Dodanie wyświetlania filmów

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php require 'includes/header.php';?>
+<?php require 'includes/header.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<?php $sql = "
+SELECT 
+    f.id,
+    title,
+    time,
+    description,
+    image,
+    d.name AS director,
+    d.surname AS director2,
+    g.name AS genre
+FROM Films as f
+JOIN Directors as d ON f.director = d.id
+JOIN Genres as g ON f.genre = g.id
+";
+
+$result = mysqli_query($conn, $sql);?>
+<section>
+<?php while($movie = mysqli_fetch_assoc($result)): ?>
+    <div>
+        <img src="https://<?= htmlspecialchars($movie['image']) ?>" alt="<?= htmlspecialchars($movie['title']) ?>">
+    </div>
+<?php endwhile;?>
+</section>
+
+
 <?php require 'includes/footer.php';?>
