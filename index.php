cb67aa6e603bdcb931f9742a20088f7ed66b5bf7
x0derus <i.karpowicz09@gmail.com>
2026-02-07 22:10:05 +0100

    Dodano slide banery filmów, poprawiono css

--- index.php
+++ index.php
@@ -15,17 +15,33 @@ SELECT
     g.name AS genre
 FROM Films as f
 JOIN Directors as d ON f.director = d.id
-JOIN Genres as g ON f.genre = g.id
+JOIN Genres as g ON f.genre = g.id LIMIT 5
 ";
 
 $result = mysqli_query($conn, $sql);?>
-<section>
-<?php while($movie = mysqli_fetch_assoc($result)): ?>
-    <div>
-        <img src="https://<?= htmlspecialchars($movie['image']) ?>" alt="<?= htmlspecialchars($movie['title']) ?>">
+<section class="slider">
+    <div class="slides">
+        <?php while ($movie = mysqli_fetch_assoc($result)): ?>
+            <div class="slide">
+                <img src="https://<?= htmlspecialchars($movie['image']) ?>" alt="<?= htmlspecialchars($movie['title']) ?>">
+            </div>
+        <?php endwhile; ?>
     </div>
-<?php endwhile;?>
 </section>
+<script>
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+let index = 0;
+
+setInterval(() => {
+    index++;
+    if (index >= totalSlides) {
+        index = 0;
+    }
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}, 4000);
+</script>
+
 
 
 <?php require 'includes/footer.php';?>
